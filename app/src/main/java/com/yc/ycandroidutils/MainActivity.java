826a8d5c1738ec76d06c78f7a885070df3ac08a4
zh <2544789777@qq.com>
2018-05-11 15:33:56 +0800

    增加glide 圆角 圆形 模糊等图片的加载

--- app/src/main/java/com/yc/ycandroidutils/MainActivity.java
+++ app/src/main/java/com/yc/ycandroidutils/MainActivity.java
@@ -3,8 +3,10 @@ package com.yc.ycandroidutils;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.yc.yclibrary.YcGlideUtils;
 import com.yc.yclibrary.YcSPUtils;
 import com.yc.yclibrary.YcToastUtils;
 
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     TextView mText1;
     @BindView(R.id.text2)
     TextView mText2;
+    @BindView(R.id.ImageView)
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.text1:
-                YcSPUtils.getInstance().put("name","名字");
+                YcSPUtils.getInstance().put("name", "名字");
+                YcGlideUtils.loadFilletImage(this, "http://img4.imgtn.bdimg.com/it/u=1665207864,746409922&fm=27&gp=0.jpg", mImageView, 10);
+//                YcGlideUtils.LoadingThumbnails(this, "http://img4.imgtn.bdimg.com/it/u=1665207864,746409922&fm=27&gp=0.jpg", 0.1f,mImageView);
                 break;
             case R.id.text2:
 
