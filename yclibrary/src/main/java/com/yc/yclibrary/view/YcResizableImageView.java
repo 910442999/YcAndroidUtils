07f8ed38d8273e8b62d0a892838db0f68e66868a
zhang <zhang@ylmf.com>
2019-01-30 18:30:04 +0800

    增加自定义imagview 根据宽  适配高度

--- yclibrary/src/main/java/com/yc/yclibrary/view/YcResizableImageView.java
+++ yclibrary/src/main/java/com/yc/yclibrary/view/YcResizableImageView.java
@@ -28,7 +28,7 @@ public class YcResizableImageView extends AppCompatImageView {
         Drawable d = getDrawable();
         if (d != null) {
             // ceil not round - avoid thin vertical gaps along the left/right edges
-            int width = MeasureSpec.getSize(widthMeasureSpec) - YcUtils.dp2px(40);
+            int width = MeasureSpec.getSize(widthMeasureSpec);
             //高度根据使得图片的宽度充满屏幕计算而得
             int height = (int) Math.ceil((float) width * (float) d.getIntrinsicHeight() / (float) d.getIntrinsicWidth());
             setMeasuredDimension(width, height);
